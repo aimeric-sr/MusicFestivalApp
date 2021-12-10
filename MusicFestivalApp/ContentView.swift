b55a38e5ab3e5ebae124d96bd072cb3072c2faa5
aimeric-sr <aimeric.sorin@epsi.fr>
2021-12-10 14:51:13 +0100

    commit functional app

--- MusicFestivalApp/ContentView.swift
+++ MusicFestivalApp/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var loginVM = LoginViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Form{
+                HStack{
+                    Spacer()
+                    Image(systemName: loginVM.isAuthenticated ? "lock.fill" : "lock.open")
+                }
+                TextField("Username", text: $loginVM.username)
+                TextField("Password", text: $loginVM.password)
+                HStack{
+                    Spacer()
+                    Button("login"){
+                        loginVM.login()
+                    }.padding()
+                    Button("sign out"){
+                        
+                    }.padding()
+                    Spacer()
+                }
+            }
+            HStack{
+                Button("Get Accounts"){
+                    
+                }
+                .padding()
+                .background(.blue)
+                .cornerRadius(10)
+                .foregroundColor(.white)
+            }
+        }
     }
 }
 
