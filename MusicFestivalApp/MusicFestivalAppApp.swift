aaf1b78578cd1b8b2a583872edd68716fe8ad080
aimeric-sr <aimeric.sorin@epsi.fr>
2021-12-10 14:43:46 +0100

    Initial Commit

---
+++ MusicFestivalApp/MusicFestivalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicFestivalAppApp.swift
+//  MusicFestivalApp
+//
+//  Created by Aimeric Sorin on 10/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct MusicFestivalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
