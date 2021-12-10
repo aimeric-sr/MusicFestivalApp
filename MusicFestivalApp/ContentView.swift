1e6565e06e498d81de6c3c4a6561439223fbecfb
aimeric-sr <aimeric.sorin@epsi.fr>
2021-12-10 19:30:18 +0100

    organization app

--- MusicFestivalApp/ContentView.swift
+++ MusicFestivalApp/ContentView.swift
@@ -8,53 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject private var loginVM = LoginViewModel()
-    @StateObject private var artistListVM = ArtistListViewModel()
-    
+   
     var body: some View {
-        NavigationView{
-            VStack{
-                Form{
-                    HStack{
-                        Spacer()
-                        Image(systemName: loginVM.isAuthenticated ? "lock.fill" : "lock.open")
-                    }
-                    TextField("Username", text: $loginVM.username)
-                    TextField("Password", text: $loginVM.password)
-                    
-                }
-                HStack{
-                    Button("login"){
-                        loginVM.login()
-                    }.padding()
-                    Button("sign out"){
-                        print("sign out pressed")
-                        loginVM.signOut()
-                    }.padding()
-                }
-                VStack{
-                    Spacer()
-                    if(loginVM.isAuthenticated){
-                        List(artistListVM.artists, id:\.id){ artist in
-                            HStack{
-                                Text("\(artist.id)")
-                                Text("\(artist.name)")
-                                Text("\(artist.nationality)")
-                                Text("\(artist.musicStyles)")
-                            }
-                        }
-                    }
-                    Spacer()
-                    Button("Get Artists"){
-                        artistListVM.getAllArtists()
-                    }
-                    .padding()
-                    .background(.blue)
-                    .cornerRadius(10)
-                    .foregroundColor(.white)
-                }
-            }.navigationTitle("Login")
-        }
+       LoginView()
     }
 }
 
---
+++ MusicFestivalApp/Views/Connection/LoginView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  MusicFestivalApp
+//
+//  Created by Aimeric Sorin on 10/12/2021.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @StateObject private var loginVM = LoginViewModel()
+    @StateObject private var artistListVM = ArtistListViewModel()
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                Form{
+                    HStack{
+                        Spacer()
+                        Image(systemName: loginVM.isAuthenticated ? "lock.fill" : "lock.open")
+                    }
+                    TextField("Username", text: $loginVM.username)
+                    TextField("Password", text: $loginVM.password)
+                    
+                }
+                HStack{
+                    Button("login"){
+                        loginVM.login()
+                    }.padding()
+                    Button("sign out"){
+                        print("sign out pressed")
+                        loginVM.signOut()
+                    }.padding()
+                }
+                VStack{
+                    Spacer()
+                    if(loginVM.isAuthenticated){
+                        List(artistListVM.artists, id:\.id){ artist in
+                            HStack{
+                                Text("\(artist.id)")
+                                Text("\(artist.name)")
+                                Text("\(artist.nationality)")
+                                Text("\(artist.musicStyles)")
+                            }
+                        }
+                    }
+                    Spacer()
+                    Button("Get Artists"){
+                        artistListVM.getAllArtists()
+                    }
+                    .padding()
+                    .background(.blue)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+                }
+            }.navigationTitle("Login")
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
